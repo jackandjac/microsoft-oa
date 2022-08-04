bccc6f5666ddb5571d4cba38e6e0530b8a71006a
junleili <jackandjac@gmail.com>
2022-08-04 12:12:30 -0400

    Leetcode 73. Set Matrix Zeroes

---
+++ src/SetMatrixZeros.java
@@ -0,0 +1,43 @@
+
+/**
+ *
+ * Leetcode 73. Set Matrix Zeroes
+ * */
+
+public class SetMatrixZeros {
+
+    public void setZeroes(int[][] matrix) {
+        boolean isCol = false;
+        int m = matrix.length, n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) isCol = true;
+
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (isCol) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
